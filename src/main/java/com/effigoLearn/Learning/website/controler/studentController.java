0f2c36eabdaefea57182f959d2ea941d662af07a
Vaibhav4228 <vaibhavsharma88890@gmail.com>
2025-01-09 10:18:28 +0530

    created student model and setuping pg sql

---
+++ src/main/java/com/effigoLearn/Learning/website/controler/studentController.java
@@ -0,0 +1,14 @@
+package com.effigoLearn.Learning.website.controler;
+
+import com.effigoLearn.Learning.website.model.studentModel;
+
+import java.util.*;
+
+public class studentController {
+    public List<studentModel> getAll() {
+
+        return null;
+    }
+
+}
+
